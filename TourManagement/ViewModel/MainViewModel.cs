f799f9bd66b66de8a9e0cbd85abeeb4a352b6cba
Duc Phuc <18521254@gm.uit.edu.vn>
2020-12-28 00:11:30 +0700

    Setup Project

---
+++ TourManagement/ViewModel/MainViewModel.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TourManagement.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        #region Chức năng Button
+        public enum CHUCNANG
+        {
+            DashBoard, ManageTour, ManageTrip, ManageCustomer, ManageStaff, ManageReport, ManageAccount
+        }
+        private int _ChucNang;
+        public int ChucNang { get => _ChucNang; set { _ChucNang = value; OnPropertyChanged(); } }
+        #endregion
+
+        #region Declare Binding Command
+        public ICommand BtnDashboardCommand { get; set; }
+        public ICommand BtnManageTourCommand { get; set; }
+        public ICommand BtnManageTripCommand { get; set; }
+        public ICommand BtnManageCustomerCommand { get; set; }
+        public ICommand BtnManageStaffCommand { get; set; }
+        public ICommand BtnManageReportCommand { get; set; }
+        public ICommand BtnManageAccountCommand { get; set; }
+
+        #endregion
+        public MainViewModel()
+        {
+            #region Binding Command
+            BtnDashboardCommand = new RelayCommand<object>((p) =>
+            {
+                //if (AccountPower == 0 || AccountPower == 1)
+                //{
+                //    MessageBoxResult result = MessageBox.Show("Bạn không đủ quyền truy cập vào chức năng này!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+                //    return false;
+                //}
+
+
+                return true;
+            }, (p) =>
+            {
+                ChucNang = (int)CHUCNANG.DashBoard;
+            });
+            BtnManageTourCommand = new RelayCommand<object>((p) =>
+            {
+                //if (AccountPower == 0 || AccountPower == 1)
+                //{
+                //    MessageBoxResult result = MessageBox.Show("Bạn không đủ quyền truy cập vào chức năng này!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+                //    return false;
+                //}
+
+
+                return true;
+            }, (p) =>
+            {
+                ChucNang = (int)CHUCNANG.ManageTour;
+            });
+            BtnManageTripCommand = new RelayCommand<object>((p) =>
+            {
+                //if (AccountPower == 0 || AccountPower == 1)
+                //{
+                //    MessageBoxResult result = MessageBox.Show("Bạn không đủ quyền truy cập vào chức năng này!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+                //    return false;
+                //}
+
+
+                return true;
+            }, (p) =>
+            {
+                ChucNang = (int)CHUCNANG.ManageTrip;
+            });
+            BtnManageCustomerCommand = new RelayCommand<object>((p) =>
+            {
+                //if (AccountPower == 0 || AccountPower == 1)
+                //{
+                //    MessageBoxResult result = MessageBox.Show("Bạn không đủ quyền truy cập vào chức năng này!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+                //    return false;
+                //}
+
+
+                return true;
+            }, (p) =>
+            {
+                ChucNang = (int)CHUCNANG.ManageCustomer;
+            });
+            BtnManageStaffCommand = new RelayCommand<object>((p) =>
+            {
+                //if (AccountPower == 0 || AccountPower == 1)
+                //{
+                //    MessageBoxResult result = MessageBox.Show("Bạn không đủ quyền truy cập vào chức năng này!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+                //    return false;
+                //}
+
+
+                return true;
+            }, (p) =>
+            {
+                ChucNang = (int)CHUCNANG.ManageStaff;
+            });
+            BtnManageReportCommand = new RelayCommand<object>((p) =>
+            {
+                //if (AccountPower == 0 || AccountPower == 1)
+                //{
+                //    MessageBoxResult result = MessageBox.Show("Bạn không đủ quyền truy cập vào chức năng này!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+                //    return false;
+                //}
+
+
+                return true;
+            }, (p) =>
+            {
+                ChucNang = (int)CHUCNANG.ManageReport;
+            });
+            BtnManageAccountCommand = new RelayCommand<object>((p) =>
+            {
+                //if (AccountPower == 0 || AccountPower == 1)
+                //{
+                //    MessageBoxResult result = MessageBox.Show("Bạn không đủ quyền truy cập vào chức năng này!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+                //    return false;
+                //}
+
+
+                return true;
+            }, (p) =>
+            {
+                ChucNang = (int)CHUCNANG.ManageAccount;
+            });
+            #endregion
+        }
+    }
+}
